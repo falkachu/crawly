823419c077720a645339ce11596e8a66e92e3a42
Falkachu <marass.simon@gmail.com>
2019-06-11 11:45:52 +0200

    Update crawly.go

--- crawly.go
+++ crawly.go
@@ -10,51 +10,55 @@ import (
 	"strings"
 )
 
+type Crawly struct{
+	URL string
+}
+
 // check for error message
-func check(err error) {
+func (c Crawly) check(err error) {
 	if err != nil {
 		log.Println(err)
 	}
 }
 
 // gUnzip unzip ziped data from byte array
-func gUnzip(zipdata io.Reader) []byte{
+func (c Crawly) gUnzip(zipdata io.Reader) []byte{
 	log.Println("unzipping data...")
 	zw, err := gzip.NewReader(zipdata)
-	check(err)
+	c.check(err)
 
 	unzipdata, err := ioutil.ReadAll(zw)
-	check(err)
+	c.check(err)
 
 	log.Println("data unziped")
 	return unzipdata
 }
 
 // parseXML parse xml byte data into struct
-func parseXml(xmldata []byte, dest interface{}){
+func (c Crawly) parseXml(xmldata []byte, dest interface{}){
 	log.Println("parsing xml...")
 	// Try to Unsmarshal XML to Struct Slice
-	check(xml.Unmarshal(xmldata, &dest))
+	c.check(xml.Unmarshal(xmldata, &dest))
 	log.Println("xml parsed")
 }
 
 // getData from URL and return the byte array
-func getData(url string) []byte{
+func (c Crawly) getData(url string) []byte{
 	log.Println("getting data from url...")
 
 	var body []byte
 
 	resp, err := http.Get(url)
-	check(err)
+	c.check(err)
 	defer resp.Body.Close()
 
 	if strings.Contains(url, ".gz"){
 		log.Println("content encoded with gzip")
-		body = gUnzip(resp.Body)
+		body = c.gUnzip(resp.Body)
 	} else {
 		log.Println("content not encoded")
 		body, err = ioutil.ReadAll(resp.Body)
-		check(err)
+		c.check(err)
 	}
 
 	log.Println("data recieved")
